bf73f6b21794c02f2d1b1335f98983d2aea4e861
Vardges <vardges.davtyan@dataart.com>
2025-05-29 17:25:45 +0400

    Routing, route guard  and a new theme

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output, } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,15 +12,24 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent {
   private accountService = inject(AccountService);
   cancelRegister = output<boolean>();
+  private toastr = inject(ToastrService)
   model: any = {}
 
   register() {
+
+    if(!this.model.username || !this.model.password)
+    {
+      this.toastr.error('Please enter both username and password.', 'Missing Fields');
+      return
+    }
+
+
     this.accountService.register(this.model).subscribe({
       next: response => {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
